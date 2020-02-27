358864f40cfba6d31627c61d897a35047cd6b71e
Cameron Chandoke <cameron.chandoke@hotmail.com>
2020-02-27 17:21:31 -0600

    Write fetch-list and day-at-the-bakery functions

--- src/introduction_to_clojure/core.clj
+++ src/introduction_to_clojure/core.clj
@@ -2,6 +2,20 @@
 (ns introduction-to-clojure.core
   (:require [bakery.core :refer :all]))
 
+(def pantry-ingredients #{:flour :sugar})
+
+(def fridge-ingredients #{:milk :egg :butter})
+
+(def squeezed #{:egg})
+
+(def scooped #{:milk :flour :sugar})
+
+(def simple #{:butter})
+
+(defn error [& args]
+  (apply println args)
+  :error)
+
 (defn add-egg []
   (grab :egg)
   (squeeze)
@@ -35,75 +49,64 @@
 
 
 (defn scooped? [ingr]
-  (cond
-    (= ingr :milk)
-    true
-    (= ingr :sugar)
-    true
-    :else
-    false))
-
+  (contains? scooped ingr))
 
 
 (defn squeezed? [ingr]
-  (cond
-    (= ingr :egg)
-    true
-    :else
-    false))
-
+  (contains? squeezed ingr))
 
 (defn simple? [ingr]
-  (= ingr :butter))
+  (contains? simple ingr))
 
 
-(defn add-scooped [ingr]
-  (if (scooped? ingr)
-    (do
-      (grab :cup)
-      (scoop ingr)
-      (add-to-bowl)
-      (release))
-      (do
-        (println ingr "is not a scoop-able ingredient")
-        :error)))
+(defn add-scooped
+  ([ingr n]
+   (if (scooped? ingr)
+     (dotimes [i n]
+       (grab :cup)
+       (scoop ingr)
+       (add-to-bowl)
+       (release))
+     (error ingr "is not a scoop-able ingredient")))
+  ([ingr]
+   (add-scooped ingr 1)))
+
+(defn add-squeezed
+  ([ingr n]
+    (if (squeezed? ingr)
+      (dotimes [i n]
+        (grab ingr)
+        (squeeze)
+        (add-to-bowl))
+      (error ingr "is not a squeezable ingredient")))
+  ([ingr]
+    (add-squeezed ingr 1)))
 
 
-
-(defn add-squeezed [ingr]
-  (if (squeezed? ingr)
-    (do
-      (grab ingr)
-      (squeeze)
-      (add-to-bowl))
-      (do
-        (println ingr "is not a squeezable ingredient")
-        :error)))
-
-
-(defn add-simple [ingr]
-  (if (simple? ingr)
-    (do
-      (grab ingr)
-      (squeeze)
-      (add-to-bowl))
-    (do
-      (println ingr "is not a simple ingredient")
-      :error)))
+(defn add-simple
+  ([ingr n]
+    (if (simple? ingr)
+     (dotimes [i n]
+       (grab ingr)
+       (add-to-bowl))
+     (error ingr "is not a simple ingredient")))
+  ([ingr]
+   (add-simple ingr 1)))
 
 
-(defn add [ingr]
-  (cond
-    (squeezed? ingr)
-    (add-squeezed ingr)
-    (scooped? ingr)
-    (add-scooped ingr)
-    (simple? ingr)
-    (add-simple ingr)
-    :else
-    (do
-      (println "Unknown ingredient:" ingr)
-      :error)))
+(defn add
+  ([ingr n]
+    (cond
+      (squeezed? ingr)
+      (add-squeezed ingr n)
+      (scooped? ingr)
+      (add-scooped ingr n)
+      (simple? ingr)
+      (add-simple ingr n)
+      :else
+      (error "Unknown ingredient:" ingr)))
+  ([ingr]
+    (add ingr 1)))
 
 
 (defn add-eggs [n]
@@ -137,12 +140,128 @@
 
 
 (defn bake-cake []
-  (add-flour-cups 2)
-  (add-eggs 2)
-  (add-milk-cups 1)
-  (add-sugar-cups 1)
+  (add :flour 2)
+  (add :egg 2)
+  (add :milk 1)
+  (add :sugar 1)
   (mix)
   (pour-into-pan)
   (bake-pan 25)
   (cool-pan))
+
+
+(defn bake-cookies []
+  (add :egg 1)
+  (add :flour 1)
+  (add :sugar 1)
+  (add :butter 1)
+  (mix)
+  (pour-into-pan)
+  (bake-pan 30)
+  (cool-pan))
+
+
+(defn from-pantry? [ingr]
+  (contains? pantry-ingredients ingr))
+
+
+(defn from-fridge? [ingr]
+  (contains? fridge-ingredients ingr))
+
+
+(defn fetch-pantry
+  ([ingr]
+   (fetch-pantry ingr 1))
+  ([ingr n]
+   (if (from-pantry? ingr)
+     (do
+       (go-to :pantry)
+       (dotimes [i n]
+         (load-up ingr))
+       (go-to :prep-area)
+       (dotimes [i n]
+         (unload ingr)))
+     (error ingr "not from pantry"))))
+
+
+(defn fetch-fridge
+  ([ingr]
+   (fetch-fridge ingr 1))
+  ([ingr n]
+   (if (from-fridge? ingr)
+     (do
+       (go-to :fridge)
+       (dotimes [i n]
+         (load-up ingr))
+       (go-to :prep-area)
+       (dotimes [i n]
+         (unload ingr)))
+     (error ingr "not from fridge"))))
+
+
+(defn fetch
+  ([ingr]
+   (fetch ingr 1))
+  ([ingr n]
+   (cond
+     (from-fridge? ingr)
+     (fetch-fridge ingr n)
+     ;;
+     (from-pantry? ingr)
+     (fetch-pantry ingr n)
+     ;;
+     :else
+     (println "I don't know where to find ingr"))))
+
+
+(defn load-up-amount [ingr amount]
+  (dotimes [i amount]
+    (load-up ingr)))
+
+
+(defn unload-amount [ingr amount]
+  (dotimes [i amount]
+    (unload ingr)))
+
+
+(def locations {:pantry pantry-ingredients
+                :fridge fridge-ingredients})
+
+
+(defn fetch-list [shopping-list]
+  (doseq [location (keys locations)]
+    (go-to location)
+    (doseq [ingr (get locations location)]
+      (load-up-amount ingr (get shopping-list ingr 0)))
+    (go-to :prep-area)
+    (doseq [ingr (get locations location)]
+      (unload-amount ingr (get shopping-list ingr 0)))))
+
+
+(defn day-at-the-bakery []
+  (doseq [order (get-morning-orders)]
+    (let [items (get order :items)]
+      (dotimes [n (get items :cake 0)]
+        (fetch-list {:egg 2
+                     :flour 2
+                     :milk 1
+                     :sugar 1})
+        (let [rack-id (bake-cake)]
+          (delivery {:orderid (get order :orderid)
+                      :address (get order :address)
+                      :rackids [rack-id]})))
+      (dotimes [n (get items :cookies 0)]
+        (fetch-list {:egg 1
+                     :flour 1
+                     :butter 1
+                     :sugar 1})
+        (let [rack-id (bake-cookies)]
+          (delivery {:orderid (get order :orderid)
+                      :address (get order :address)
+                      :rackids [rack-id]}))))))
+
+
+(defn main []
+  (day-at-the-bakery))
+
 
